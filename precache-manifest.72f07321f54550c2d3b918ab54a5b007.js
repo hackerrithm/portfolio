f4af3ca6d44262ac7414069ee3f9e39bb5c62dad
Kemar Galloway <kemargalloway@gmail.com>
2020-07-17 19:24:29 -0500

    Updates

---
+++ precache-manifest.72f07321f54550c2d3b918ab54a5b007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f438c7a6cf4bed36dbafac06a8e7dd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "706a55ca537e5b111d94",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27d20b033e7b8ad2ebf4",
+    "url": "/portfolio/static/js/2.2f480398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2f480398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706a55ca537e5b111d94",
+    "url": "/portfolio/static/js/main.c7c26043.chunk.js"
+  },
+  {
+    "revision": "aa08a9928dce12b6427a",
+    "url": "/portfolio/static/js/runtime-main.f1db4333.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
